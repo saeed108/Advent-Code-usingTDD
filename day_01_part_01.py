0a46c57b5a945d6640a7fcd086813263611c9f11
Saeed Sadeghi <saeed.sadeghighahroodi@chasacademy.se>
2023-03-12 20:20:36 +0100

    complleting codes day01_part_01

--- day_01_part_01.py
+++ day_01_part_01.py
@@ -1,7 +1,30 @@
-
+import os
 def read_file(file_path:str):
-    pass
+    address="{}.txt".format(file_path)
+    if os.path.exists(address):
+        file=open(address , "r")
+        txt=file.read()
+        lines=txt.split("\n")
+        if "" in lines:
+            lines.remove("")
+        numbers=list(map(int , lines))
+        return numbers
+    
+    else:
+        return False
 
 
 def calculate_fuel(mass_list: list):
-    pass
+    sum=0
+    for item in mass_list:
+        sum+=(item//3)-2
+    
+    return sum
+
+
+
+fuels=read_file("input_01")
+result=calculate_fuel(fuels)
+print(result)
+
+
